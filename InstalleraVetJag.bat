de7ae1304d9d56ea0d8e2d6902ef26ad4f93789f
Fr4gzzzzz <80005176+Fr4gzzzzz@users.noreply.github.com>
2021-07-10 22:44:18 +0200

    Add files via upload

---
+++ InstalleraVetJag.bat
@@ -0,0 +1,50 @@
+@echo off
+title FATAL ERROR
+echo WARNING! A VIRUS HAS BEEN DETECTED!!!
+pause
+echo WINDOWS PROTECTION ERROR!!! PRESS ANY KEY TO RUN ANTIVIRUS SCAN!!!
+pause
+echo DETECTED CORRUPTED FILES!!! ANTIVIRUS SCAN….
+echo Atempt 1 Failed
+echo Atempt 2 Failed
+echo Atempt 3 Failed
+echo Atempt 4 Failed
+echo Atempt 5 Failed
+echo Atempt 6 Failed
+echo Atempt 7 Failed
+echo Atempt 8 Failed
+pause
+echo PROTECTION FAILED!!!
+echo PROTECTION FAILED!!!
+echo PROTECTION FAILED!!!
+echo ALL FILES IN THIS COMPUTER WILL BE DELETED!!!
+echo ALL FILES IN THIS COMPUTER WILL BE DELETED!!!
+echo ALL FILES IN THIS COMPUTER WILL BE DELETED!!!
+pause
+:1
+color 01
+dir c:\windows\system32/s
+color 02
+dir c:\windows\system32/s
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo JAG BARA SKOJA HAHAHAHAHAHA 🙂
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .kodad av Alex
+pause
